215c32a62b93355cf6419a49bd1f910917a9640f
kushal1997 <kushalrao103@gmail.com>
2023-07-23 16:07:28 +0530

    Form Validation,email js setup done

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,38 +1,53 @@
-import React, { useState } from 'react'
-import { useRef } from 'react';
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import emailjs from 'emailjs-com';
 import './contact.css';
-import emailjs from '@emailjs/browser';
 import { useTitle } from '../../components/hooks/useTitle';
+
+const indianMobileNumberRegExp = /^[7-9][0-9]{9}$/;
+const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const validationSchema = yup.object().shape({
+  name: yup.string()
+    .min(4, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: yup
+    .string()
+    .matches(emailRegExp, 'Invalid email')
+    .required('Email is required'),
+  mob: yup
+    .string()
+    .matches(indianMobileNumberRegExp, 'Invalid mobile number')
+    .required('Mobile number is required'),
+});
 
 export const Contact = () => {
   useTitle("Contact Page");
-  const form = useRef();
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
-  const sendEmail = (e) => {
-
-    e.preventDefault();
-
-    emailjs.sendForm('service_1rd7neg', 'template_38602kx', form.current, 'WJ5B0393l_yNlS5Kf')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
-  };
-
-  const [submitted, setSubmitted] = useState(false);
-
-
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    sendEmail();
-    setSubmitted(!submitted);
-  }
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      mob: '',
+    },
+    validationSchema,
+    onSubmit: (values, { resetForm }) => {
+      // Submit the form data to EmailJS
+      emailjs.send('service_1rd7neg', 'template_38602kx', values, 'WJ5B0393l_yNlS5Kf')
+        .then(() => {
+          console.log('Email sent successfully!');
+          resetForm();
+          setShowConfirmation(true);
+        })
+        .catch((error) => {
+          console.error('Error sending email:', error);
+        });
+    },
+  });
   return (
-
-
-    <main className=" p-3 h-screen bg-[#030321]">
+    <main className=" p-2 mt-1 h-screen bg-[#030321]">
       <div className='flex flex-col justify-center items-center ml-[10rem]  p-1 w-[90rem]'>
 
         <div className='contact  '>
@@ -63,33 +78,60 @@ export const Contact = () => {
 
           <div className='flex justify-center mt-10'>
             <div className='container'>
-              <div className={submitted ? 'hidden' : 'show'}>
-                <form ref={form} onSubmit={handleFormSubmit}>
-                  <h1 className=' text-center text-2xl underline'>Registration form</h1>
+              {showConfirmation ? (
+                <div className='maindiv flex flex-col justify-center items-center'>
 
-                  <div className='ui-form'>
-                    <div className='field'>
-                      <label>Full Name</label>
-                      <input className='input' type='text' name='from_name' placeholder='Full Name' />
-                      <label>Email Id </label>
-                      <input className='input' type='Email' name='email_add' placeholder='Email Id' />
-                      <label>Phone </label>
-                      <input className='input' type='tel' name='ph_num' placeholder='Phone no' />
-
-                    </div>
-
-                    <button class="contact_form text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" type="submit">
-                      SUBMIT
-                    </button>
-
-
+                  <img width="100" height="100" src="https://img.icons8.com/external-sbts2018-flat-sbts2018/100/external-thank-you-basic-ui-elements-2.3-sbts2018-flat-sbts2018.png" alt="external-thank-you-basic-ui-elements-2.3-sbts2018-flat-sbts2018" />
+                  <h1>Thank You for your message!</h1>
+                  <p className='text-l'>We'll get back to you soon.</p>
+                </div>
+              ) : (
+                <form onSubmit={formik.handleSubmit}>
+                  <div>
+                    <label htmlFor="name">Name</label>
+                    <input
+                      className='input'
+                      id="name"
+                      name="name"
+                      type="text"
+                      placeholder='Your Name'
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.name}
+                    />
+                    {formik.touched.name && formik.errors.name && <div className=' text-red-500 font-semibold'>{formik.errors.name}</div>}
                   </div>
-                </form>
-              </div>
-
-              <div className={submitted ? 'show' : 'hidden'}>
-                <h1>Thank You For Submition</h1>
-              </div>
+                  <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                      className='input'
+                      id="email"
+                      name="email"
+                      type="email"
+                      placeholder='email@example.com'
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.email}
+                    />
+                    {formik.touched.email && formik.errors.email && <div className=' text-red-500 font-semibold'>{formik.errors.email}</div>}
+                  </div>
+                  <div>
+                    <label htmlFor="mob">Phone Num</label>
+                    <input
+                      className='input'
+                      name="mob"
+                      type="tel"
+                      placeholder='9999999999'
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.mob}
+                    />
+                    {formik.touched.mob && formik.errors.mob && <div className=' text-red-500 font-semibold'>{formik.errors.mob}</div>}
+                  </div>
+                  <button class="contact_form text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" type="submit">
+                    SUBMIT
+                  </button>
+                </form>)}
 
 
             </div>
@@ -100,12 +142,7 @@ export const Contact = () => {
             <p className='text-white hover:text-blue-500'>Branch Ofc : B-19,Bhubaneswar</p>
             <p className='text-white hover:text-blue-500'>Odisha,751007</p>
 
-
-            {/* <div className='flex justify-center h-56 items-center border-3 border-solid border-sky-500 w-80'> */}
-
             <div className=' ml-5 mt-4 ' >
-
-
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7484.503512911215!2d85.8454036!3d20.289843900000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a19a14fd5712c61%3A0x9b02a71f624c28dd!2sDaya%20Consultancy%20Services%20Pvt.%20Ltd.!5e0!3m2!1sen!2sin!4v1689834516745!5m2!1sen!2sin"
                 width={300}
@@ -120,6 +157,5 @@ export const Contact = () => {
         </div>
       </div>
     </main>
-
   )
 }
--- src/pages/other/ReactForm.js
+++ src/pages/other/ReactForm.js
@@ -1,22 +1,47 @@
-import React, { useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import emailjs from 'emailjs-com';
 import "./form.css"
 
+const validationSchema = yup.object().shape({
+    from_name: yup.string()
+        .min(5, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Your name is equired'),
+    from_email: yup.string().email('Invalid email').required('Email is required'),
+    message: yup.string().min(10, 'Too Short!').required('Message is required'),
+});
+
 export const ReactForm = () => {
-    const form = useRef();
+    const [showConfirmation, setShowConfirmation] = useState(false);
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+    const formik = useFormik({
+        initialValues: {
+            from_name: '',
+            from_email: '',
+            message: '',
+            que_num: '',
+            sub_name: '',
+        },
+        validationSchema,
+        onSubmit: (values, { resetForm }) => {
+            emailjs
+                .send('service_1rd7neg', 'template_rkl15l4', values, 'WJ5B0393l_yNlS5Kf')
+                .then(() => {
+                    console.log('Email sent successfully!');
+                    setShowConfirmation(true);
+                    resetForm();
+                })
+                .catch((error) => {
+                    console.error('Error sending email:', error);
+                });
+        },
+    });
 
-        emailjs.sendForm('service_1rd7neg', 'template_rkl15l4', form.current, 'WJ5B0393l_yNlS5Kf')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
+    const handleCloseConfirmation = () => {
+        setShowConfirmation(false);
     };
-
-
 
     const [selectedTopic, setSelectedTopic] = useState('');
 
@@ -32,33 +57,64 @@ export const ReactForm = () => {
     return (
 
         <>
-                        
 
-                        <div className='maindiv'>
-                            <p className=' text-white text-3xl flex justify-center underline'>Feedback Form</p>
-                            <form ref={form} onSubmit={sendEmail} className='ownForm'>
-                                <label>Name</label>
-                                <input type="text" name="from_name" />
-                                <label>Email</label>
-                                <input type="email" name="from_email" />
-                                <div className='flex flex-row'>
-                                    <div className='flex flex-col'>
-                                        <label>Topic</label>
-                                        <select name='topic_name' id="" onChange={(e) => { setSelectedTopic(e.target.value) }}>
-                                            {
-                                                topics.map((e) => {
-                                                    return (
-                                                        <option key={e}>{e}</option>
-                                                    )
 
-                                                })
-                                            }
-                                        </select>
-                                    </div>
-                                    <div className='flex flex-col ml-5'>
-                                        <label>Sub Topic</label>
 
-                                        {selectedTopic && <select name="sub_name" id="">
+
+            <div className='maindiv'>
+                {!showConfirmation ? (
+                    <div>
+                        <p className=' text-white text-3xl flex justify-center underline'>Feedback Form</p>
+                        <form onSubmit={formik.handleSubmit} className='ownForm'>
+                            <label htmlFor='from_name'>Name</label>
+                            <input
+                                type="text"
+                                name="from_name"
+                                placeholder='Your Name'
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.from_name}
+                            />
+                            {formik.touched.from_name && formik.errors.from_name && <div className=' text-red-500 font-semibold text-left'>{formik.errors.from_name}</div>}
+
+                            <label htmlFor='from_email'>Email</label>
+                            <input
+                                type="email"
+                                name="from_email"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.from_email}
+                            />
+                            {formik.touched.from_email && formik.errors.from_email && <div className=' text-red-500 font-semibold text-left'>{formik.errors.from_email}</div>}
+
+                            <div className='flex flex-row'>
+                                <div className='flex flex-col'>
+                                    <label htmlFor='topic_name'>Topic</label>
+                                    <select
+                                        name='topic_name'
+                                        value={formik.values.topic_name}
+                                        onChange={(e) => { setSelectedTopic(e.target.value) }}
+                                    >
+                                        {
+                                            topics.map((e) => {
+                                                return (
+                                                    <option key={e}>{e}</option>
+                                                )
+
+                                            })
+                                        }
+                                    </select>
+                                </div>
+                                <div className='flex flex-col ml-5'>
+                                    <label htmlFor='sub_name'>Sub Topic</label>
+
+                                    {selectedTopic &&
+                                        <select
+                                            name="sub_name"
+                                            onChange={formik.handleChange}
+                                            onBlur={formik.handleBlur}
+                                            value={formik.values.sub_name}
+                                        >
                                             {
                                                 subMenus[selectedTopic].map(menu => {
                                                     return <option key={menu}>{menu}</option>
@@ -66,21 +122,55 @@ export const ReactForm = () => {
                                             }
                                         </select>}
 
-                                    </div>
-                                    <div className='flex flex-col ml-5'>
-                                        <label>Que Num</label>
-                                        <input className=' h-[23px] mt-0' type="number" name="que_num" />
-                                    </div>
                                 </div>
+                                <div className='flex flex-col ml-5'>
+                                    <label htmlFor='que_num'>Que Num</label>
+                                    <input
+                                        className=' h-[23px] mt-0 w-[100px]'
+                                        type="number"
+                                        name="que_num"
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
+                                        value={formik.values.que_num}
+                                    />
+                                </div>
+                            </div>
 
 
-                                <label>Message</label>
-                                <textarea className=' indent-2 max-h-fit min-h-[10rem]' type="text" name="message" />
+                            <label htmlFor='message'>Message</label>
+                            <textarea
+                                className=' indent-2 max-h-fit min-h-[10rem]'
+                                type="text"
+                                name="message"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.message}
+                            />
+                            {formik.touched.message && formik.errors.message && <div className=' text-red-500 font-semibold text-left'>{formik.errors.message}</div>}
+                            <button
+                                id='submit'
+                                type="submit"
+                                value='submit'
+                                className=" text-xl text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2"
+                            >
+                                <b>Submit</b>
+                            </button>
+                        </form>
+                    </div>
+                ) : (
+                    <div className='flex flex-col justify-center items-center'>
+                        <img width="100" height="100" src="https://img.icons8.com/external-sbts2018-flat-sbts2018/100/external-thank-you-basic-ui-elements-2.3-sbts2018-flat-sbts2018.png" alt="external-thank-you-basic-ui-elements-2.3-sbts2018-flat-sbts2018" />
+                        <h1 className='text-white mt-2 text-2xl'>Thank you for your Feedback!</h1>
+                        <button
+                            onClick={handleCloseConfirmation}
+                            className=" mt-4 text-l text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2"
+                        >
+                            Close
+                        </button>
+                    </div>
+                )}
+            </div>
 
-                                <button id='submit' type="submit" value='submit' className=" text-xl text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2"><b>Submit</b></button>
-                            </form>
-                        </div>
-           
         </>
 
 
